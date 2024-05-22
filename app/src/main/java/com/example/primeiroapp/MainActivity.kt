58d748f8e0462a0dbb71f1ab770ce8802b24c2dc
lucarl07 <junitinho2007@gmail.com>
2024-05-22 19:59:45 -0300

    Initialize Git repository on finished project

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.primeiroapp
+
+import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeiroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.msgButton.setOnClickListener {
+            val nome: String = binding.inputName.text.toString()
+            binding.msgOutput.text = "Olá, $nome!"
+        }
+    }
+}
